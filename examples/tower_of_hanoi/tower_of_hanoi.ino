9a0340245858fdddf136079ae362e7d80857ac56
Gruning A  Dr (Computer Science) <a.gruning@surrey.ac.uk>
2018-05-27 22:17:03 +0100

    comment code

--- examples/tower_of_hanoi/tower_of_hanoi.ino
+++ examples/tower_of_hanoi/tower_of_hanoi.ino
@@ -1,6 +1,6 @@
 /** 
     Code to operate the Tower of Hanoi Robot from the FT Computing kit.
-    Copyright 2016, 2018 Andre Gruening
+    Copyright 2018 Andre Gruening
     GNU Licence v3 or later
 */
 
@@ -18,21 +18,29 @@ int serial_putchar(char ch, FILE* f) {
 
 FILE serial;
 
+/// angle of tower
 const Sensor& angle = ft.ex;
+/// motor to rotate tower
 const Motor& rotate = ft.motor1;
+
+/// motor to lift arm
 const Motor& lift = ft.motor2;
-const Motor& magnet = ft.motor3;
+/// indicate bottom position of arm
 const Input& bottom = ft.e3;
+/// indicate top position of arm
 const Input& top = ft.e4;
 
-const unsigned maxDuty = 96;
-
+/// pwm driver to the electromagnet
+const Motor& magnet = ft.motor3;
 
 //!   We have three poles to stack discs on
-enum {LEFT, MIDDLE, RIGHT, NUM_POLES};
+enum {COPPAR, SILVER, GOLD, NUM_POLES};
 
 //! We have this many discs in total. Discs are ordered in size and labled by their order in size.
 #define NUM_DISCS 5
+
+//! tolarance for pole position
+#define TOLERANCE 3
 
 /** A Pole with a stack of discs on it. */
 struct Pole {
@@ -40,6 +48,7 @@ struct Pole {
   int stack[NUM_DISCS];
   /** indicates the topmost used position of the pole, ie the number of discs on it **/
   int top;
+  /// tower angle for this pole
   const int angle;
   Pole(const int _angle): top(0), angle(_angle) {};
 
@@ -77,20 +86,29 @@ struct Pole {
   }
 };
 
-/// Tower of Hanoi has NUM_POLES poles at the give angles:
+/** Tower of Hanoi has NUM_POLES poles at the give angles:
+    Pole angles should be checked by trail and error with the help of test.ino
+ */
 Pole poles[NUM_POLES] = { Pole(300), Pole(500), Pole(680)};
 
+/**
+   rotate tower to position of pole.
+   @param pole pole to rotate to.
+ */
 void rotate_to(const Pole& pole) {
 
   rotate.left();
-  while(angle.getReading() > pole.angle + 3);
+  while(angle.getReading() > pole.angle + TOLERANCE);
 
   rotate.right();
-  while(angle.getReading() < pole.angle - 3);
+  while(angle.getReading() < pole.angle - TOLERANCE);
   
   rotate.off();
 }
-  
+
+/**
+   lower arm to bottom position.
+ */
 void lower_arm() {
 
   lift.down();
@@ -98,7 +116,9 @@ void lower_arm() {
   lift.off();
 }
 
-
+/**
+   raise arm to top position
+ */
 void raise_arm() {
 
   lift.up();
@@ -107,13 +127,21 @@ void raise_arm() {
 }
 
 /**
-   Move robot arm to pick up a disc at from and put it down at to
+   Move robot arm to pick up a disc at from and put it down at
+   to. In the course of this operation the magnet is switched on with
+   a strength appropriate for the disc (ie lighter discs get less
+   magnetic field strength.
+
    @param disc id for disc -- to adjust magnetic strength.
    @param from pole to take disc from
    @param to pole to put disc down on.
 
-   Pre-condition: arm is in the upper position.
-   Post-condition: arm is in the upper position.
+   Pre-condition: arm is in the top position.
+   Post-condition: arm is in the top position.
+
+   This is the only function that couples the controler with the model
+   (in this case the ft robot hardware).
+
  */
 void robot_move_disc(const int disc, const int from, const int to) {
 
@@ -141,10 +169,7 @@ void robot_move_disc(const int disc, const int from, const int to) {
   lower_arm();
   magnet.off();
   raise_arm();
-  
 }
-
-
 
 /** 
     move a single disc from one pole to another.
@@ -152,38 +177,36 @@ void robot_move_disc(const int disc, const int from, const int to) {
     @param to pole to put the disc on
 */
 void move_single_disc(const int from, const int to) {
+
   const int disc = poles[from].take();
   poles[to].put(disc);
   fprintf(stderr, "Moving Disk %i from %i to %i.\n", disc, from, to);
   robot_move_disc(disc, from, to);
-  /** robot control code to go in here */
 }
 
-
 /**
-   recusrively move a number of disc from one pole to another, keeping the
-   invarients of the Tower of Hanoi problem.
+   recusrively move a number of discs from one pole to another, keeping the
+   invariants of the Tower of Hanoi problem.
 
-   @param from pole to take the discs from
-   @param free pole to use to temporarily put discs.
+   @param from pole to take the discs from.
+   @param free pole to use to temporarily store discs.
    @param to pole to put the discs on
-   @param number of topmost discs to move from from to to.
+   @param number of discs to move from from to to.
  */
 void move_discs(const int from, const int free, const int to, const int number) {
 
-  // done -- no disc to move
+  // done -- no discs to move
   if(number == 0) return;
 
   // recursively move the topmost number-1 discs to the free pole
   move_discs(from, to, free, number-1);
 
-  // move the remaining disc direct:
+  // move the remaining single disc direct:
   move_single_disc(from, to);
 
-  // recurively move the disc temporatily stored on the free pole to
+  // recursively move the discs temporarily stored on the free pole to
   // the to pol.
   move_discs(free, from, to, number-1);
-
 }
 	
 void setup() {
@@ -195,17 +218,17 @@ void setup() {
   stdout = &serial;
   stderr = &serial;
   
-  // Fill LEFT pole with NUM_DISCS discs:
+  // Fill COPPAR pole with NUM_DISCS discs:
   for(int i = NUM_DISCS; i > 0; i--) {
-    poles[LEFT].put(i);
+    poles[COPPAR].put(i);
   }
 
 #ifdef TEST
-  // lower_arm();
-  raise_arm();
-  rotate.right();
-  printf("Setup done\n");
 
+  lower_arm();
+  raise_arm();
+  rotate_to(pole[COPPAR]);
+  rotate_to(pole[GOLD]);
   exit(0);
 #endif
 
@@ -215,10 +238,10 @@ void setup() {
 
 void loop() {
 
-  // move all discs from the LEFT to the RIGHT pole, using the MIDDLE
-  // pole to temporarily store discs.
-  move_discs(LEFT, MIDDLE, RIGHT, NUM_DISCS);
+  /* move all discs from the COPPAR to the GOLD pole, using the SILVER
+     pole to temporarily store discs. 
+  */
+  move_discs(COPPAR, SILVER, GOLD, NUM_DISCS);
 
   exit(0);
-  //move_discs(RIGHT, MIDDLE, LEFT, NUM_DISCS);
 }
